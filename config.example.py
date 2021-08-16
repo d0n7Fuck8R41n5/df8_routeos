0d78be8f4f9dd037aa92237350b5ce50edec2999
d0n7Fuck8r41n5 <d0n7Fuck8r41n5@protonmail.com>
2021-08-16 23:02:03 +0500

    added get all rule from router

--- config.example.py
+++ config.example.py
@@ -3,5 +3,6 @@ tlg_user_id = ''  # You telegram user id
 route_ip = ''  # IP router
 route_user = ''  # router user
 route_password = ''  # router password
+get_rule_from_router = False
 rules = []  # Rules array
 
