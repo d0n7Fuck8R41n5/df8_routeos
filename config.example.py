f66b358074b0b0ea9fa6c6040cfb521c391b1f3b
d0n7Fuck8r41n5 <d0n7Fuck8r41n5@protonmail.com>
2021-08-16 22:27:56 +0500

    first commit

---
+++ config.example.py
@@ -0,0 +1,7 @@
+tlg_token = ''  # Telegram bot token
+tlg_user_id = ''  # You telegram user id
+route_ip = ''  # IP router
+route_user = ''  # router user
+route_password = ''  # router password
+rules = []  # Rules array
+
